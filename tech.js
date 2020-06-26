4d5dc42b65ad4854c511125b1f1d9dbc4b8b8ace
iceskatercodes <aravindisiceskater@gmail.com>
2020-06-26 10:15:58 +0530

    Create tech.js

---
+++ tech.js
@@ -0,0 +1,50 @@
+let technicalSpecifications =[{
+    Dimensions:{
+        "overallLength":3500,
+        "overallWidth":1600,
+        "overallHeight":1600,
+        "wheelBase":1400,
+        "trackWidth":{
+            "front":1000,
+            "rear":2000
+        },
+        "maximumTurningRadius":4.5,
+        "maximumGroundClearence":170
+        },
+        Capacities:{
+            "seatingCapacity":5,
+            "fueltankCapacity":20
+        },
+        Engine:{
+            "engineType":"string",
+            "numberOfCylinders":5,
+            "numberOfValves":12,
+            "capacity":"800cc",
+            "bore x stroke":"73.0x79.5",
+            "compressionRatio":"10:1",
+            "MaximumPower":"67/620",
+            "MaximumTorque":"90/3500",
+            "fuelDistribution":"Multipoint Injection"
+        },
+        Transmission:{
+            "type":"5 speed-Mt"
+        },
+        chasis:{
+            "steering":"Rack & Pinion",
+            "breaks":{
+                "front":"string",
+                "back":"string"
+            },
+            "suspension":{
+                "front":"string",
+                "back":"string"
+            },
+            "shockAbsorbs":"Gas Filled",
+            "tyre":"String"
+        },
+        weight:{
+            "krebWeight":"860-880",
+            "groddVeichleWeight":"string"
+        }
+}]
+console.log(technicalSpecifications)
